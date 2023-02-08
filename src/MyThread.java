4a0676dc53cadc5b88aaa50bf189267108df379f
luc-nguyen'174 <louiskingvn@gmail.com>
2023-02-08 23:36:09 +0700

    done

---
+++ src/MyThread.java
@@ -0,0 +1,12 @@
+public class OddThread{
+    void run(int n1,int n2){
+        for ( int i = n1; i <10 ; i+=2){
+            System.out.println(i);
+            try {
+                Thread.sleep(n2);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
