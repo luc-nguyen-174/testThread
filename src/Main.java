4d90cf7afac051fa35777da61f0c11bf8f85850f
luc-nguyen'174 <louiskingvn@gmail.com>
2023-02-09 21:59:43 +0700

    reup

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyThread mt = new MyThread();
+
+        Thread t1 = new Thread(){
+            public void run() {
+                mt.myThread(1,10);
+            }
+        };
+        Thread t2 = new Thread(){
+            public void run() {
+                mt.myThread(0,15);
+            }
+        };
+        t1.start();
+        try {
+            t1.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        t2.start();
+
     }
 }
--- src/MyThread.java
+++ src/MyThread.java
@@ -1,6 +1,6 @@
-public class OddThread{
-    void run(int n1,int n2){
-        for ( int i = n1; i <10 ; i+=2){
+public class MyThread {
+    void myThread(int n1, int n2) {
+        for (int i = n1; i <= 10; i += 2) {
             System.out.println(i);
             try {
                 Thread.sleep(n2);
